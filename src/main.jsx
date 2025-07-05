3c8bf6c5e70d7255b311bcaa8ac92222f84ab250
Cassiel <Cassielcdl@gmail.com>
2025-07-05 20:16:52 -0300

    Ajustes na paginação

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './css/index.css'
 import './css/tailwind.css'
 import './css/tailwind-build.css'
-import Home from './Pages/landing/Home.jsx';
+import Home from './Home.jsx';
 import SignUp from './Pages/signUp/SignUp.jsx';
 import SignIn from './Pages/signIn/SignIn.jsx';
 import Dashboard from './Pages/dashboard/Dashboard.jsx';
-import Checkout from './Pages/Checkout/Checkout.jsx';
+import Checkout from './Pages/checkout/Checkout.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/landing" element={<Home />} />
+      <Route path="/" element={<Home />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/signin" element={<SignIn />} />
       <Route path="/dashboard" element={<Dashboard />} />

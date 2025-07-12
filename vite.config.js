4a596bdd3636ed897bd7b9baa610d22f8a4f4443
Cassiel <Cassielcdl@gmail.com>
2025-07-12 16:39:58 -0300

    Corrige config do Vite para ambiente de produção

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
+  server: process.env.NODE_ENV === 'development' ? {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://urbanity.onrender.com',
         changeOrigin: true,
         rewrite: path => path.replace(/^\/api/, ''),
       },
     },
-  },
+  } : undefined,
 });
